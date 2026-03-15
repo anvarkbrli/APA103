3a128511669e1b41b727282e3b38f55266c85ba3
anvarkbrli <anvarmk-apa103@code.edu.az>
2026-03-15 13:46:27 +0400

    array resize

---
+++ 01-C#IntroMethods/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 1 - Riyazi emeller
+            //Console.WriteLine("Ilk ededi daxil edin");
+            //double num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Ikinci ededi daxil edin");
+            //double num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{num1}" + "+" + $"{num2}" + " = " + Addititon(num1, num2));
+            //Console.WriteLine($"{num1}" + "-" + $"{num2}" + " = " + Substraction(num1, num2));
+            //Console.WriteLine($"{num1}" + "/" + $"{num2}" + " = " + Division(num1, num2));
+            //Console.WriteLine($"{num1}" + "*" + $"{num2}" + " = " + Multiplication(num1, num2));
+            //Console.WriteLine($"{num1}" + "^" + $"{num2}" + " = " + Power(num1, num2));
+
+            //Task 2 - Tek ve cut ededlerin tapilmasi
+            //OddNEven(14, 20, 35, 40, 57, 60, 100);
+
+            //Task 3 - arrayda 4 ve 5 e bolunen ededlerin cemi
+            //int[] numbers = { 14, 20, 35, 40, 57, 60, 100 };
+            //Console.WriteLine(ArrayMethod(numbers));
+
+            //Task 4 - Daaxil edilmis cumledeki daxil edilmis simvol sayi
+            //Console.WriteLine("Cumlenizi daxil edin:");
+            //string sentence = Console.ReadLine();
+            //Console.WriteLine("Simvol daxil edin:");
+            //char symbol = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(CountSymbol(sentence,symbol));
+
+        }
+
+        //Task 1 - Riyazi emeller
+        //public static double Addititon(double num1, double num2)
+        //{
+        //    double netice = num1 + num2;
+        //    return netice;
+        //}
+
+        //public static double Substraction(double num1, double num2)
+        //{
+        //    double netice = num1 - num2;
+        //    return netice;
+        //}
+
+        //public static double Multiplication(double num1, double num2)
+        //{
+        //    double netice = num1 * num2;
+        //    return netice;
+        //}
+
+        //public static double Division(double num1, double num2)
+        //{
+        //    double netice = num1 / num2;
+        //    return netice;
+        //}
+
+        //public static double Power(double num1, double num2)
+        //{
+        //    double netice = Math.Pow(num1,num2);
+        //    return netice;
+        //}
+
+
+        //Task 2 - tek ve cut ededlerin tapilamasi
+        //static void OddNEven(params int[] arr)
+        //{
+        //    Console.Write("Tek ededler: ");
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] % 2 != 0)
+        //        {
+        //            Console.Write(arr[i] + " ");
+        //        }
+        //    }
+        //}
+        //    Console.WriteLine();
+        //    Console.Write("Cut ededler: ");
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] % 2 == 0)
+        //        {
+        //            Console.Write(arr[i] + " ");
+        //        }
+        //    }
+
+        //Task 3 - arrayda 4 ve 5 e bolunen ededlerin cemi
+        //public static int ArrayMethod(params int[] numbers)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        if (numbers[i] % 4 == 0 && numbers[i] % 5 == 0)
+        //        {
+        //            sum += numbers[i];
+
+        //        }
+        //        else
+        //        {
+        //            sum += 0;
+
+        //        }
+        //    }
+        //    return sum;  
+        //}
+
+        //Task 4 - Daaxil edilmis cumledeki daxil edilmis simvol sayi
+        //public static int CountSymbol(string sentence, char symbol)
+        //{
+        //    int symbolCount = 0;
+
+        //    for (int i = 0; i < sentence.Length; i++)
+        //    {
+        //        if (symbol == sentence[i])
+        //        {
+        //            symbolCount++;
+        //        }
+
+        //    }
+        //    return symbolCount;
+        //}
+
+    }
+    }
+
---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02_HeapStackRefOutArrayResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 34, 76, 88 };
+            int[] resizedArray = ArrayResize(numbers, 11, 22, 33, 44, 48, 96, 12);
+            for (int i=0; i< resizedArray.Length; i++)
+            {
+                Console.Write(resizedArray[i] + " ");
+            }
+        }
+          
+        public static int[] ArrayResize(int[] array, params int[] newElements)
+        {
+            int[] newArray = new int[array.Length + newElements.Length];
+
+            for (int i = 0; i <newArray.Length; i++)
+            {
+                if (i < array.Length)
+                {
+                    newArray[i] = array[i];
+                }
+                else
+                {
+                    newArray[i] = newElements[i - array.Length];
+                }
+
+            }
+            return newArray;
+            
+        }
+    }
+}
