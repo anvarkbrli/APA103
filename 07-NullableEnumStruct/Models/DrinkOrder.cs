68e5d8d1e10347b1315e17c8b85c6b304380f981
anvarkbrli <anvarmk-apa103@code.edu.az>
2026-03-28 11:34:01 +0400

    cafe order system

---
+++ 07-NullableEnumStruct/Models/DrinkOrder.cs
@@ -0,0 +1,117 @@
+﻿using _07_NullableEnumStruct.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_NullableEnumStruct.Models
+{
+    public class DrinkOrder
+    {
+        public int OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DrinkType Drink { get; set; }
+        public DrinkSize Size { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Price { get; set; }
+        public DrinkOrder(int orderNumber, string customerName, DrinkType drink, DrinkSize size, OrderStatus status =  OrderStatus.New)
+        {
+            OrderNumber = orderNumber;
+            CustomerName = customerName;
+            Drink = drink;
+            Size = size;
+        }
+
+        public decimal CalculatePrice()
+        {
+            switch (Drink)
+            {
+                case DrinkType.Coffee:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small:
+                            return Price = 3;
+                           
+                        case DrinkSize.Medium:
+                            return Price = 4;
+                            
+                        case DrinkSize.Large:
+                            return Price = 5;
+                            
+
+                    }
+                    break;
+                    
+                case DrinkType.Tea:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small:
+                            return Price = 2;
+                            
+                        case DrinkSize.Medium:
+                            return Price = 3;
+                            
+                        case DrinkSize.Large:
+                            return Price = 4;
+                            
+
+                    }
+                    break;
+
+                case DrinkType.Juice:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small:
+                            return Price = 4;
+                            
+                        case DrinkSize.Medium:
+                            return Price = 5;
+                            
+                        case DrinkSize.Large:
+                            return Price = 6;
+                            
+
+                    }
+                    break;
+
+                case DrinkType.Water:
+                    switch (Size)
+                    {
+                        case DrinkSize.Small:
+                            return Price = 1;
+                            
+                        case DrinkSize.Medium:
+                            return Price = 1.5M;
+                            
+                        case DrinkSize.Large:
+                            return Price = 2;
+                            
+                    }
+                    break;
+
+            }
+            return 0;
+
+        }
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            Status = newStatus;
+            Console.WriteLine($"Sifaris : {OrderNumber}, statusu: {Status}");
+        }
+        public void DisplayOrder()
+        {
+            CalculatePrice();
+            Console.WriteLine("Siferis detallari");
+            Console.WriteLine($"Sifrais Nomresi: {OrderNumber}");
+            Console.WriteLine($"Musterinin adi: {CustomerName}");
+            Console.WriteLine($"Icki novu: {Drink}");
+            Console.WriteLine($"Ickinin olcusu: {Size}");
+            Console.WriteLine($"Sifarisin statusu: {Status}");
+            Console.WriteLine($"Odenecek mebleg: {Price} AZN");
+
+        }
+    }
+}
---
+++ 07-NullableEnumStruct/Program.cs
@@ -0,0 +1,71 @@
+﻿using _07_NullableEnumStruct.Enums;
+using _07_NullableEnumStruct.Models;
+
+namespace _07_NullableEnumStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinkOrder order1 = new(101, "Ali", DrinkType.Coffee, DrinkSize.Medium);
+            //order1.DisplayOrder();
+            //order1.UpdateStatus(Enums.OrderStatus.Preparing);
+            //order1.UpdateStatus(Enums.OrderStatus.Ready);
+            //order1.UpdateStatus(Enums.OrderStatus.Delivered);
+
+            DrinkOrder order2 = new(102, "Leyla", DrinkType.Tea, DrinkSize.Large);
+            //order2.DisplayOrder();
+            //order2.UpdateStatus(Enums.OrderStatus.Ready);
+
+            DrinkOrder order3 = new(103, "Vuqar", DrinkType.Juice, DrinkSize.Small);
+            //order3.DisplayOrder();
+
+            //Getvalues metodlari
+            //foreach (DrinkType drink in Enum.GetValues(typeof(DrinkType)))
+            //{
+            //    Console.WriteLine(drink);
+            //}
+
+            //foreach (DrinkSize size in Enum.GetValues(typeof(DrinkSize)))
+            //{
+            //    Console.WriteLine(size);
+            //}
+
+            //foreach (OrderStatus status in Enum.GetValues(typeof(OrderStatus)))
+            //{
+            //    Console.WriteLine(status);
+            //}
+
+            //ToString()
+            //Console.WriteLine(DrinkType.Coffee.ToString());
+            //Console.WriteLine(DrinkSize.Large.ToString());
+
+            //Parse
+            //DrinkType drink = (DrinkType)Enum.Parse(typeof(DrinkType), "Tea");
+            //DrinkSize size = (DrinkSize)Enum.Parse(typeof(DrinkSize), "Medium");
+            //Console.WriteLine(drink);
+            //Console.WriteLine(size);
+
+            //Statistika
+            Console.WriteLine("Umumi sifaris sayi: 3");
+
+            Console.WriteLine("Ilk sifarisin qiymeti:");
+            Console.WriteLine(order1.CalculatePrice() + " AZN");
+
+            Console.WriteLine("Ikinci sifarisin qiymeti:");
+            Console.WriteLine(order2.CalculatePrice() + " AZN");
+
+            Console.WriteLine("Ucuncu sifarisin qiymeti:");
+            Console.WriteLine(order3.CalculatePrice() + " AZN");
+
+            decimal orderPrice1 = order1.CalculatePrice();
+            decimal orderPrice2 = order2.CalculatePrice();
+            decimal orderPrice3 = order3.CalculatePrice();
+            decimal TotalPrice = orderPrice1 + orderPrice2 + orderPrice3;
+
+
+            Console.WriteLine($"Umumi mebleg: " + TotalPrice + " AZN");
+        }
+
+    }
+}
